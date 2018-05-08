c6312d2c82510d81b75838f0c5e3174cd979cea3
Bradley Ayers <bradley.ayers@gmail.com>
2018-05-08 15:53:59 +1000

    chore: fix tests

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,14 @@
 import { addDecorator, configure } from "@storybook/react";
 import { decorator } from "./decorator";
 
+// Must be performed before `require.context()`, see
+// https://github.com/storybooks/storybook/issues/3246#issuecomment-374790016
+addDecorator(decorator);
+
 const req = require.context("../src", true, /.*\.stories.tsx$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
-addDecorator(decorator);
 configure(loadStories, module);
