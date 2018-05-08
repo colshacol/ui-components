40bf51fc80b9e274f7c473a70735b32c8f78eb68
Bradley Ayers <bradley.ayers@gmail.com>
2018-05-08 14:55:25 +1000

    fix(AnimateSlideDown): add opacity fade in, fix bezier curve

--- src/components/AnimateSlideDown/index.tsx
+++ src/components/AnimateSlideDown/index.tsx
@@ -4,24 +4,31 @@ import { classes } from "typestyle";
 import { style } from "typestyle-react";
 import { WithClassName } from "../WithClassName";
 
-const duration = 250;
+const duration = 200;
 type STATE = "unmounted" | "exited" | "entering" | "entered" | "exiting";
 
 export const AnimateSlideDown = ({ children }: { children?: React.ReactElement<{ className?: string }> }) => (
-  <CSSTransition appear={true} timeout={0} in={true} classNames={{}}>
-    {(state: STATE) => (
-      <WithClassName className={classes(AnimatedTransform, state === "entered" ? appearActiveClassName : null)}>
-        {children}
-      </WithClassName>
-    )}
+  <CSSTransition appear={true} timeout={32} in={true} classNames={{}}>
+    {(state: STATE) => {
+      console.log(state);
+      return (
+        <WithClassName
+          className={classes(state === "entering" ? enteringClass : null, state === "entered" ? enteredClass : null)}
+        >
+          {children}
+        </WithClassName>
+      );
+    }}
   </CSSTransition>
 );
 
-const AnimatedTransform = style({
-  transition: `transform ${duration}ms cubic-bezier(.2, .45, 0, 1)`,
-  transform: "translate3d(0, -8px, 0)"
+const enteringClass = style({
+  transform: "translate3d(0, -8px, 0)",
+  opacity: 0
 });
 
-const appearActiveClassName = style({
-  transform: "translate3d(0, 0, 0)"
+const enteredClass = style({
+  transition: `transform ${duration}ms cubic-bezier(0.1, 0.45, 0.25, 1), opacity ${duration}ms cubic-bezier(0.1, 0.45, 0.25, 1)`,
+  transform: "translate3d(0, 0, 0)",
+  opacity: 1
 });
