38d0e111c18bfc91af198568d02ecd5b07c1f6c2
Benjamin Humphrey <humphreybc@gmail.com>
2018-06-04 11:51:05 +1000

    refactor(OnHover): Remove id / counter

--- src/components/OnHover/index.tsx
+++ src/components/OnHover/index.tsx
@@ -7,11 +7,7 @@ export interface Props {
   onMouseLeave: (e: React.MouseEvent<HTMLSpanElement>) => void;
 }
 
-let counter: number = 0;
-
 export class OnHover extends React.PureComponent<Props, {}> {
-  private readonly id = this.props.id !== undefined ? this.props.id : `OnHover_${counter++}`;
-
   private readonly onMouseEnter = (e: React.MouseEvent<HTMLSpanElement>) => {
     this.props.onMouseEnter(e);
   };
@@ -23,7 +19,7 @@ export class OnHover extends React.PureComponent<Props, {}> {
   public render() {
     return (
       <span
-        id={this.id}
+        id={this.props.id}
         onMouseEnter={this.onMouseEnter}
         onMouseLeave={this.onMouseLeave}
         style={{ display: "flex", maxWidth: "100%" }}
