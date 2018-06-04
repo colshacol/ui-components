451692273cfe8be42b3fda9638fbcd5f5851b876
Benjamin Humphrey <humphreybc@gmail.com>
2018-06-04 10:37:55 +1000

    fix(IconSpinner): Update size to match other icons

--- src/components/Icons/IconSpinner.tsx
+++ src/components/Icons/IconSpinner.tsx
@@ -6,16 +6,16 @@
 import { createSvg, Props } from "@heydovetail/svg-to-react";
 import * as React from "react";
 
-export default createSvg((width = 32, height = 32, color): React.ReactElement<Props> => (
-  <svg width={width} height={height} viewBox="0 0 38 38" stroke={color}>
-    <g transform="translate(1 1)" strokeWidth={2} fill="none" fillRule="evenodd">
-      <circle strokeOpacity={0.1} cx={18} cy={18} r={18} />
-      <path d="M36 18c0-9.94-8.06-18-18-18">
+export default createSvg((width = 24, height = 24, color): React.ReactElement<Props> => (
+  <svg width={width} height={height} viewBox="0 0 24 24" stroke={color}>
+    <g fill="none" fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} transform="translate(2 2)">
+      <circle strokeOpacity={0.1} cx={10} cy={10} r={10} />
+      <path d="M20 10c0-5.523-4.477-10-10-10">
         <animateTransform
           attributeName="transform"
           type="rotate"
-          from="0 18 18"
-          to="360 18 18"
+          from="0 10 10"
+          to="360 10 10"
           dur="1.5s"
           repeatCount="indefinite"
         />
