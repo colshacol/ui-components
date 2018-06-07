98963fa20af94a1af721106d79cf2f09c966576c
Bradley Ayers <bradley.ayers@gmail.com>
2018-06-07 10:07:26 +1000

    chore(Color): convert to typestyle-react

--- src/components/Color/index.tsx
+++ src/components/Color/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
-import { style } from "typestyle";
+import { styled } from "typestyle-react";
 import { BORDER_RADIUS, BOX_SHADOW_FOCUS, COLORS } from "../../styles";
-import CacheComputed from "../../util/CacheComputed";
 import IconCheckMini from "../Icons/IconCheckMini";
 import IconMinus from "../Icons/IconMinus";
 
@@ -16,40 +15,36 @@ export class Color extends React.PureComponent<Props> {
   public render() {
     const { color, onClick, selected = false, strikethrough = false } = this.props;
     return (
-      <button className={colorStyleClassCache.get(color)} onClick={onClick}>
+      <ColoredButton onClick={onClick} styled={{ color }}>
         {selected ? <IconCheckMini /> : null}
         {strikethrough ? <IconMinus color="#C4C2CB" /> : null}
-      </button>
+      </ColoredButton>
     );
   }
 }
 
-const colorStyleClassCache = new CacheComputed<string, string>(
-  color => color,
-  color =>
-    style({
-      $nest: {
-        "&:hover": {
-          cursor: "pointer",
-          opacity: 0.9
-        },
-        "&:active": {
-          opacity: 1
-        },
-        "&:focus": {
-          boxShadow: BOX_SHADOW_FOCUS
-        }
-      },
+const ColoredButton = styled("button", (props: { color: string }) => ({
+  $nest: {
+    "&:hover": {
+      cursor: "pointer",
+      opacity: 0.9
+    },
+    "&:active": {
+      opacity: 1
+    },
+    "&:focus": {
+      boxShadow: BOX_SHADOW_FOCUS
+    }
+  },
 
-      alignItems: "center",
-      backgroundColor: color,
-      borderRadius: BORDER_RADIUS,
-      border: "none",
-      color: COLORS.white,
-      display: "flex",
-      height: "32px",
-      justifyContent: "center",
-      outline: "none",
-      width: "32px"
-    })
-);
+  alignItems: "center",
+  backgroundColor: props.color,
+  borderRadius: BORDER_RADIUS,
+  border: "none",
+  color: COLORS.white,
+  display: "flex",
+  height: "32px",
+  justifyContent: "center",
+  outline: "none",
+  width: "32px"
+}));
